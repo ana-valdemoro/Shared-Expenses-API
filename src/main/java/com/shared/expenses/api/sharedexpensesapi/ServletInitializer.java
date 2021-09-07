34a7d7c8c577477893f06eba5bcac472d5404a8a
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-09-07 18:52:08 +0100

    Rename initial clases

--- src/main/java/com/shared/expenses/api/sharedexpensesapi/ServletInitializer.java
+++ src/main/java/com/shared/expenses/api/sharedexpensesapi/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SharedExpensesApiApplication.class);
+		return application.sources(SharedExpensesApplication.class);
 	}
 
 }
