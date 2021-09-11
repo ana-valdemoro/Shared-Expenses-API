4ecd9c7886ed069c9bf66278c5de14efe483c242
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-09-11 20:33:17 +0100

    Method getBalance add to class friend

--- src/main/java/com/shared/expenses/api/sharedexpensesapi/Models/Friend.java
+++ src/main/java/com/shared/expenses/api/sharedexpensesapi/Models/Friend.java
@@ -1,23 +1,29 @@
 package com.shared.expenses.api.sharedexpensesapi.Models;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import com.shared.expenses.api.sharedexpensesapi.Repositories.ExpenseRepository;
+import com.shared.expenses.api.sharedexpensesapi.Repositories.FriendRepository;
 
 @Entity(name = "Friends")
 public class Friend {
+    // private FriendRepository friendRepository;
+    // private ExpenseRepository expenseRepository;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column( insertable = false ,updatable = false, nullable = false) Long id;
+    @Column( insertable = false ,updatable = false, nullable = false) 
+    private Long id;
     @Column
     private String name;
     @Column
     private String lastName;
 
-
-    public Friend(){}
+    public Friend (){}
     public Friend(String name, String lastName){
         this.name = name;
         this.lastName = lastName;
@@ -26,7 +32,17 @@ public class Friend {
     public Long getId(){ return id;}
     public String getLastName(){ return lastName;}
     public String getName(){return name;}
-    // public float getSaldo(){
-    //     return
-    // }
+
+    private float sumExpenses(List<Expense> expenses){
+        float totalExpenses = 0.00f;
+        for (Expense expense : expenses){
+            totalExpenses += expense.getValue();
+        }
+        return totalExpenses;
+    }
+
+    public float getBalance(List<Friend> friends, List<Expense> debts, List<Expense> individualContributions){
+        float expensePerFriend = this.sumExpenses(debts) / friends.size();
+        return this.sumExpenses(individualContributions) - expensePerFriend;
+    }
 }
