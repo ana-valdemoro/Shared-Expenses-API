5f2576d6f70bc61ce3555d21dd809b8045d07536
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-09-11 11:05:12 +0100

    Refactor to friendRepository

--- src/main/java/com/shared/expenses/api/sharedexpensesapi/Repositories/FriendRepository.java
+++ src/main/java/com/shared/expenses/api/sharedexpensesapi/Repositories/FriendRepository.java
@@ -1,11 +1,8 @@
 package com.shared.expenses.api.sharedexpensesapi.Repositories;
 
-import java.util.Optional;
-
+import java.util.List;
 import com.shared.expenses.api.sharedexpensesapi.Models.Friend;
 
 public interface FriendRepository extends BaseRepository<Friend, Long>{
-    Optional<Friend> findById(Long primaryKey);
-
-    Iterable<Friend> findAll(); 
+    List<Friend> findAll(); 
 }
