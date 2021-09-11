fd3fe73b6908ca719a2dd8363013779ed9c8c59f
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-09-11 20:35:00 +0100

    Create BalanceService

--- src/main/java/com/shared/expenses/api/sharedexpensesapi/Controllers/SharedExpensesController.java
+++ src/main/java/com/shared/expenses/api/sharedexpensesapi/Controllers/SharedExpensesController.java
@@ -4,9 +4,8 @@ package com.shared.expenses.api.sharedexpensesapi.Controllers;
 import java.util.List;
 
 import com.shared.expenses.api.sharedexpensesapi.Models.Balance;
-import com.shared.expenses.api.sharedexpensesapi.Models.SharedExpensesGroup;
-import com.shared.expenses.api.sharedexpensesapi.Repositories.ExpenseRepository;
-import com.shared.expenses.api.sharedexpensesapi.Repositories.FriendRepository;
+import com.shared.expenses.api.sharedexpensesapi.Services.BalanceService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/sharedexpenses")
 public class SharedExpensesController {
-    private final SharedExpensesGroup expensesGroup;
+    private final BalanceService balanceService;
 
-    SharedExpensesController(FriendRepository friendRepository, ExpenseRepository expenseRepository){
-        this.expensesGroup =  new SharedExpensesGroup(friendRepository, expenseRepository);
+    SharedExpensesController(BalanceService balanceService){
+      this.balanceService = balanceService;
     }
     @GetMapping
 	public List<Balance> getBalance() {
-		return this.expensesGroup.getBalances();
+		return this.balanceService.getBalances();
 	}
 
 }
---
+++ src/main/java/com/shared/expenses/api/sharedexpensesapi/Services/BalanceService.java
@@ -0,0 +1,35 @@
+package com.shared.expenses.api.sharedexpensesapi.Services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shared.expenses.api.sharedexpensesapi.Models.Balance;
+import com.shared.expenses.api.sharedexpensesapi.Models.Expense;
+import com.shared.expenses.api.sharedexpensesapi.Models.Friend;
+import com.shared.expenses.api.sharedexpensesapi.Repositories.ExpenseRepository;
+import com.shared.expenses.api.sharedexpensesapi.Repositories.FriendRepository;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BalanceService {
+    private FriendRepository friendRepository;
+    private ExpenseRepository expenseRepository;
+    
+    public BalanceService(FriendRepository friendRepository, ExpenseRepository expenseRepository){
+        this.friendRepository = friendRepository;
+        this.expenseRepository = expenseRepository;
+    }
+
+    public List<Balance> getBalances(){
+        List<Friend> friends = this.friendRepository.findAll();
+        List<Expense> expenses = expenseRepository.findByOrderByDateDesc();
+        List<Balance> balanceList= new ArrayList<Balance>();
+        for(Friend friend : friends){
+            List<Expense> individualExpenses = expenseRepository.findByFriendId(friend.getId());
+            Balance balance = new Balance(friend, friend.getBalance(friends, expenses, individualExpenses));
+            balanceList.add(balance);
+        }
+        return balanceList;
+    }
+}
