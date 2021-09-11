dfcd2574db867b56de174244e21af1738efa3ff3
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-09-11 11:15:48 +0100

    add method findByFriendId to ExpenseRepository

--- src/main/java/com/shared/expenses/api/sharedexpensesapi/Repositories/ExpenseRepository.java
+++ src/main/java/com/shared/expenses/api/sharedexpensesapi/Repositories/ExpenseRepository.java
@@ -1,9 +1,13 @@
 package com.shared.expenses.api.sharedexpensesapi.Repositories;
 
+import java.util.List;
 import com.shared.expenses.api.sharedexpensesapi.Models.Expense;
 
 
 public interface ExpenseRepository extends BaseRepository<Expense, Long> {
-    Iterable<Expense> findByOrderByDateDesc();
+    List<Expense> findByOrderByDateDesc();
+
+    List<Expense> findByFriendId(Long id);
+
 
 }
