ef675fa55895dff47e40e76238d77fa0767933a5
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-09-11 11:04:34 +0100

    Expenses controller done

--- src/main/java/com/shared/expenses/api/sharedexpensesapi/Controllers/ExpenseController.java
+++ src/main/java/com/shared/expenses/api/sharedexpensesapi/Controllers/ExpenseController.java
@@ -1,27 +1,32 @@
 package com.shared.expenses.api.sharedexpensesapi.Controllers;
 
+
 import com.shared.expenses.api.sharedexpensesapi.Models.Expense;
 import com.shared.expenses.api.sharedexpensesapi.Repositories.ExpenseRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/expenses")
 public class ExpenseController {
-    private final ExpenseRepository repository;
+    private final ExpenseRepository expenseRepository;
     
-    ExpenseController(ExpenseRepository repository){
-        this.repository = repository;
+    ExpenseController(ExpenseRepository expenseRepository){
+        this.expenseRepository = expenseRepository;
+
     }
 
     @GetMapping
 	public Iterable<Expense> getAlIterable() {
-		return repository.findByOrderByDateDesc();
+		return expenseRepository.findByOrderByDateDesc();
 	}
 
-    // @PostMapping
-    // public Expense createFriend(@RequestBody Expense expense) {
-    //     return repository.save(friend);
-    // }
+    @PostMapping
+    public Expense createFriend(@RequestBody Expense expense) {
+        return expenseRepository.save(expense);
+    }
+
 }
