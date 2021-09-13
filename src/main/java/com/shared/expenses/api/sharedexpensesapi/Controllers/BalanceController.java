b081b6dfbc44164263300272b95182cf8280d830
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-09-13 09:05:58 +0100

    Refactor from Balance controller

--- src/main/java/com/shared/expenses/api/sharedexpensesapi/Controllers/BalanceController.java
+++ src/main/java/com/shared/expenses/api/sharedexpensesapi/Controllers/BalanceController.java
@@ -1,21 +1,18 @@
 package com.shared.expenses.api.sharedexpensesapi.Controllers;
 
-
 import java.util.List;
-
 import com.shared.expenses.api.sharedexpensesapi.Models.Balance;
 import com.shared.expenses.api.sharedexpensesapi.Services.BalanceService;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sharedexpenses")
-public class SharedExpensesController {
+@RequestMapping("/balances")
+public class BalanceController {
     private final BalanceService balanceService;
 
-    SharedExpensesController(BalanceService balanceService){
+    BalanceController(BalanceService balanceService){
       this.balanceService = balanceService;
     }
     @GetMapping
